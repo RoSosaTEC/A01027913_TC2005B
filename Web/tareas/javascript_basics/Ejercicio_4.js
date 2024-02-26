c0c7f698107acc9584bbab167f9877bd4f34b675
Rodrigo Sosa <A01027913@tec.mx>
2024-02-25 22:57:51 -0600

    Ejercicio 4

--- Web/tareas/javascript_basics/Ejercicio_4.js
+++ Web/tareas/javascript_basics/Ejercicio_4.js
@@ -1,4 +1,11 @@
 function mayus(string){
-	let mayusculas=[]
-	
+	let splitWords = string.split(" ")
+	for (let i = 0; i < splitWords.length; i++){
+		splitWords[i] = splitWords[i][0].toUpperCase() + splitWords[i].substring(1)
+	}
+	let newString = splitWords.join(" ")
+	console.log(newString)
 }
+
+let pruebaString = "este es un string de prueba en minusculas"
+mayus(pruebaString)
